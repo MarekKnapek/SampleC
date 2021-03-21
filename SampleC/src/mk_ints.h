b9792201de8bd227bebea5060b57e2775e4c7484
Marek Knápek <knapek.mar@gmail.com>
2021-03-21 21:41:52 +0100

    Add mk_sint64_t and mk_uint64_t types.

--- SampleC/src/mk_ints.h
+++ SampleC/src/mk_ints.h
@@ -6,4 +6,31 @@ typedef signed int mk_sint32_t;
 typedef unsigned int mk_uint32_t;
 
 
+struct mk_sint64_parts_s
+{
+	unsigned int m_lo;
+	signed int m_hi;
+};
+typedef struct mk_sint64_parts_s mk_sint64_parts_t;
+union mk_sint64_u
+{
+	mk_sint64_parts_t m_parts;
+	double m_double;
+};
+typedef union mk_sint64_u mk_sint64_t;
+
+struct mk_uint64_parts_s
+{
+	unsigned int m_lo;
+	unsigned int m_hi;
+};
+typedef struct mk_uint64_parts_s mk_uint64_parts_t;
+union mk_uint64_u
+{
+	mk_uint64_parts_t m_parts;
+	double m_double;
+};
+typedef union mk_uint64_u mk_uint64_t;
+
+
 #endif
